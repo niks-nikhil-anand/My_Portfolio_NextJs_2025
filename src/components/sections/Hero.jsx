20aea7344cd3115a6fc7bdda5f49790e94d4867a
niks-nikhil-anand <niks.anand.developer@gmail.com>
2024-12-21 19:38:06 +0530

    Modify the Hero Section

--- src/components/sections/Hero.jsx
+++ src/components/sections/Hero.jsx
@@ -20,12 +20,11 @@ const Hero = () => {
             <div className="caption small-2 uppercase text-p3">
               Web Dev.
             </div>
-            <h1 className="mb-6 h1 text-p4 uppercase max-lg:mb-7 max-lg:h2 max-md:mb-4 max-md:text-5xl max-md:leading-12">
-              Amazingly simple
+            <h1 className="mb-6 h6 text-p4  max-lg:mb-7 max-lg:h2 max-md:mb-4 max-md:text-5xl max-md:leading-12">
+            Hi, I'm Nikhil Anand
             </h1>
             <p className="max-w-440 mb-14 body-1 max-md:mb-10">
-              We designed XORA AI Video Editor to be an easy to use, quick to
-              learn, and surprisingly powerful.
+            A passionate freelancer and website developer, crafting user-friendly and visually stunning web solutions tailored to your needs.
             </p>
 
 
